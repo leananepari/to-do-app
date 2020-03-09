897f05f1c57519618ee6cc3dd59009c7aefff625
Leana <leananeparidze.com>
2020-03-09 15:04:06 -0700

    adds login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import './css/styles.css';
 import { Route, Switch } from "react-router-dom";
+import Dashboard from './components/main/Dashboard';
+import Login from './components/authentication/Login';
+import SignUp from './components/authentication/SignUp';
 
 function App() {
   return (
     <div>
       <Switch>
-        
+        <Route exact path='/' component={Dashboard} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/signup' component={SignUp} />
       </Switch>
     </div>
   );
